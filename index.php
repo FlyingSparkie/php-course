631146e122855de8fce1c393984815a46632d9e7
FlyingSparkie <flying.sparkie@yahoo.co.uk>
2020-04-16 12:58:59 +0100

    Update index.php
    Conclusion with function and variable parsing

--- index.php
+++ index.php
@@ -10,32 +10,35 @@
 
 	echo "<h1>Welcome to ".$name."!</h1>";
 	echo "<h2>You have $".$credit." in your wallet.</h2>";
-		$products['Computer']=750;
-	    $products['Car']=15000;
-    	$products['iPhone']=1000;
-    	$products['Toaster']=75;
-    	//echo "<p>A car costs $".$products['Car']."</p>";
-    foreach($products as $key => $value){
-	    echo "<p>The ".$key." costs ".$value."</p>";
-    }
-    echo "<h2>These are the Items you can afford</h2>";
-
-	foreach($products as $key => $value){
-  	if($value <= $credit ){
-  		echo "<p>".$key."</p>"; 
-  		}
-	}
-	$amount=800;
+	$products['Computer']=750;
+	$products['Car']=15000;
+    $products['iPhone']=1000;
+    $products['Toaster']=75;
+ 
+    $amount=800;
 	$taxRate=0.0825;
 	$addedTax= $amount*$taxRate;  //amount = 800, tax = .0825
 	echo $addedTax;
-	
+    foreach($products as $key => $value){
+	echo "<p>The ".$key." costs ".$value."</p>";
+    }
+
 	function tax_calc($amount, $tax){
-		$calculate_tax = $amount*$tax;
-		$amount = round($amount+$calculate_tax,2);
-		return $amount;
-		echo tax_calc(750,0.223); // When you refresh your page you should the value 917.25 at the bottom of the screen.
-		}
+		$added_tax = $amount*$tax;
+		$amount_with_tax = round($amount+$added_tax,2);
+		return $amount_with_tax;
+	}
+	foreach($product as $key => $value){
+		$cost_with_tax = tax_calc($value,$taxRate);
+		echo"<p>The ".$key." costs ".$cost_with_tax." with tax</p>";
+	}
+	echo "<h2>These are the Items you can afford</h2>";
+	foreach($products as $key => $value){
+  		if($value <= $credit ){
+  		echo "<p>".$key."</p>"; 
+  	}
+	}
+
     ?>
   </body>
 </html>
