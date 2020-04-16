93b9bc668ef57afde19a2c506456bfe8edcd811c
FlyingSparkie <flying.sparkie@yahoo.co.uk>
2020-04-16 02:58:48 +0100

    Update index.php
    adding variables

--- index.php
+++ index.php
@@ -5,9 +5,12 @@
   </head>
   <body>
     <?php
+    $name = "PHP Store";
+    $credit = 1000;
     // Using two forward slashes we create comments in php.
     // Within the php tag, we can insert values into our HTML.
-    echo "<h1>Welcome!</h1>";
+	echo "<h1>Welcome to ".$name."!</h1>";
+	echo "<h2>You have $".$credit." in your wallet.</h2>";";
     ?>
   </body>
 </html>
